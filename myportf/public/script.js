3784c3d8ee349907fc147729ef3547faf8570288
Petr Stoklas <petrstoklas@gmail.com>
2019-01-27 15:23:44 +0100

    contact anim added

--- myportf/public/script.js
+++ myportf/public/script.js
@@ -1,5 +1,7 @@
 const backfin = document.querySelector('#backfin');
 const whole = document.querySelector('.shrk');
+let contacts = document.querySelector('.contact');
+console.log(contacts.classList);
 
 
 
@@ -16,3 +18,11 @@ const stopswim = () => {
 
 whole.addEventListener('mouseenter', swim);
 whole.addEventListener('mouseleave', stopswim);
+
+
+window.addEventListener('scroll', () => { 
+    let offset = window.pageYOffset;
+    if (offset > 800) {
+        contacts.classList.add('scrolled');
+        console.log(contacts.classList);
+    } })
